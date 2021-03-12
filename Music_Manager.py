27852459093e7e055ace45f6e533c843e958f95c
RaphaelFrisano <raphael@frisano.net>
2021-03-12 16:10:45 +0100

    Script Rework
    Started Logic Rework but already identifying all favourites through www.chosic.com and Iterating through the data to add to already existing Playlists.

--- Music_Manager.py
+++ Music_Manager.py
@@ -8,7 +8,7 @@ from spotipy import util
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
-def get_playlist_tracks(sp, username,playlist_id):
+def get_playlist_tracks(sp, username, playlist_id):
     results = sp.user_playlist_tracks(username,playlist_id)
     tracks = results['items']
     while results['next']:
@@ -29,6 +29,44 @@ if token:
     sp = spotipy.Spotify(auth=token)
 else:
     print("Can't get token for", username)
+
+
+# Initialisation
+favouritesTracks = get_playlist_tracks(sp, username, "1W62xrI7rdqkrkT0xgbwTM")
+DanceVibesPlaylist = "47wDz8QxSAsDOLB8c8HdCd"
+HopPopVibesPlaylist = "tT9IzMkySrGHwNmhBBnB5w"
+ClassicVibesPlaylist = "31159C1hoG2ZqZxtTLvBk2"
+EasyVibesPlaylist = "7Gq1sXY7RZ3LSKm86bPn7v"
+RockyVibesPlaylist = "67sSpThGjgTPiRnZ1S8GIW"
+VibeCheckPlaylist = "4GtrzqGPZdCKA29WQWlRdJ"
+DanceVibes = None
+HopPopVibes = None
+ClassicVibes = None
+EasyVibes = None
+RockyVibes = None
+VibeCheck = None
+Songnum = 0
+SongnumEnd = 999
+
+
+# Manage Music into Playlists
+while Songnum < SongnumEnd:
+    Songnum = Songnum + 1
+    ParentGenres = "Cell from Excel"
+    genres = ParentGenres.split(",")
+    for genre in genres:
+        if genre -like "Dance/Electronic":
+            DanceVibes += favouritesTracks[Songnum]
+        elif genre -like "Pop" or genre -like "HipHop"
+            HopPopVibes += favouritesTracks[Songnum]
+        elif genre -like "Classical":
+            ClassicVibes += favouritesTracks[Songnum]
+        elif genre -like "Easy Listening":
+            EasyVibes += favouritesTracks[Songnum]
+        elif genre -like "Rock" or genre -like "Metal": 
+            RockyVibes += favouritesTracks[Songnum]
+        else
+            VibeCheck += favouritesTracks[Songnum]
 
 #Connect to playlist
 trackslist = sp.current_user_saved_tracks(limit=50, offset=0)
